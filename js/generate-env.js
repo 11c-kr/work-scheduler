1774dd8439fbeea6300aa82170de841488e44339
11c-austin <152970222+tin11c@users.noreply.github.com>
2024-11-26 11:14:36 +0900

    generate-env

--- js/generate-env.js
+++ js/generate-env.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 // Netlify 환경변수를 읽어오는 스크립트
 const envContent = `
 window.ENV = {
-  ACCESS_KEY: "${process.env.ACCESS_KEY}"
+  ACCESS_KEY: "${process.env.ACCESS_KEY}",
+  LOCAL_END_POINT: "${process.env.LOCAL_END_POINT}",
+  PROD_END_POINT: "${process.env.PROD_END_POINT}"
 };
 `;
 
